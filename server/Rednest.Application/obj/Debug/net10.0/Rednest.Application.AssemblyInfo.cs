661536325e6d0701bc615e1a3ff50bf5b49c5bdb
elchinima <elchinima@code.edu.az>
2026-08-20 18:45:53 +0400

    Add Fortune component styles and update project assembly configurations

--- server/Rednest.Application/obj/Debug/net10.0/Rednest.Application.AssemblyInfo.cs
+++ server/Rednest.Application/obj/Debug/net10.0/Rednest.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rednest.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1e1c53d729dd249361f6f04f913e37ef441a84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd73973e51455e48f49e30f38f693a94916d213")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rednest.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rednest.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server/Rednest.Infrastructure/obj/Debug/net10.0/Rednest.Infrastructure.AssemblyInfo.cs
+++ server/Rednest.Infrastructure/obj/Debug/net10.0/Rednest.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rednest.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1e1c53d729dd249361f6f04f913e37ef441a84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd73973e51455e48f49e30f38f693a94916d213")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rednest.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rednest.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
