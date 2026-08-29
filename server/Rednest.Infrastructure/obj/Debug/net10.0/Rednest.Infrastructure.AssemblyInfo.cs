984582f4c7aedc6000d3afcee945138f7e820de9
elchinima <elchinima@code.edu.az>
2026-08-29 20:42:00 +0400

    Update Loader

--- server/Rednest.Infrastructure/obj/Debug/net10.0/Rednest.Infrastructure.AssemblyInfo.cs
+++ server/Rednest.Infrastructure/obj/Debug/net10.0/Rednest.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rednest.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5349a8acacebd1cb732a502e21b17325cbf3968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb4cda9ae438ff3d5a5611a43af477d87e349ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rednest.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rednest.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
