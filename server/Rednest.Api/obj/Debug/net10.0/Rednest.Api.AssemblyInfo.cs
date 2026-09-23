4ff157168ea7b44e0850f39125d4f570dfda7cd0
elchinima <elchinima@code.edu.az>
2026-09-23 17:38:53 +0400

    Fix responsive bug for mobile devices

--- server/Rednest.Api/obj/Debug/net10.0/Rednest.Api.AssemblyInfo.cs
+++ server/Rednest.Api/obj/Debug/net10.0/Rednest.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rednest.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc16f3eacd769948db918b308eaebe7a1f8afd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3214aebc04ba43ff6f31af2b095a51f4cace2b32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rednest.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rednest.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
